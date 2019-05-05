1bbb4669888765701e43201860d99499d148399d
12143 <12143@LAPTOP-MP38RBBF.fios-router.home>
2019-05-05 11:24:49 -0400

    Modified java program.

--- FirstProject/src/com/jpmc/accounts/MyFirstProgram.java
+++ FirstProject/src/com/jpmc/accounts/MyFirstProgram.java
@@ -5,6 +5,6 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello, this is soumya");
+		System.out.println("hello all, this is madura");
 	}
-
 }
