f30a77971e21b1e683bbbb4863131e81cffefae9
madir <madir@192.168.0.14>
2019-05-05 11:08:44 -0400

    Testing project

---
+++ FirstProject/src/com/jpmc/accounts/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.jpmc.accounts;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello, this is soumya");
+	}
+
+}
